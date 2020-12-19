ae85c9e8fdcfe95e32dae6ee73356573531fa3db
Joshua Dinh <joshuahungdinh@gmail.com>
2020-12-19 09:28:10 -0800

    added files

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Button from "./Components/Button/Button";
 import Nav from "./Components/Sidebar/Nav";
 import ArrowLeftIcon from "@material-ui/icons/ArrowLeft";
 import img from "./imgs/img.jpg";
-// import video2 from "./imgs/flower2.mp4";
-// import video3 from "./imgs/flower3.mp4";
 import MouseParticles from "react-mouse-particles";
 import covid19 from "./imgs/covid-vid.mov";
 import bitcoin from "./imgs/bitcoin-vid.mov";
@@ -62,59 +60,14 @@ const App = () => {
     }
   };
 
-  // const displayBackgroundVideo = () => {
-  //   if (currentIdx == 0) {
-  //     return (
-  //       <video
-  //         className="animate__animated animate__fadeInRightBig"
-  //         autoPlay
-  //         muted
-  //         loop
-  //       >
-  //         <source src={video2} type="video/mp4" />
-  //       </video>
-  //     );
-  //   }
-  //   if (currentIdx == 1) {
-  //     return (
-  //       <div>
-  //         <video
-  //           className="animate__animated animate__fadeInRightBig"
-  //           autoPlay
-  //           muted
-  //           loop
-  //         >
-  //           <source src={video} type="video/mp4" />
-  //         </video>
-  //       </div>
-  //     );
-  //   }
-  //   if (currentIdx == 2) {
-  //     return (
-  //       <>
-  //         <video
-  //           className="animate__animated animate__fadeInRightBig"
-  //           autoPlay
-  //           muted
-  //           loop
-  //         >
-  //           <source src={video3} type="video/mp4" />
-  //         </video>
-  //       </>
-  //     );
-  //   }
-  //   if (currentIdx > 2) {
-  //     setCurrentIdx(0);
-  //   }
-  //   if (currentIdx < 0) {
-  //     setCurrentIdx(2);
-  //   }
-  // };
-
   return (
     <div className="App">
+      <div className="app__opening-header typewriter">
+        <h4>Hello My Name Is</h4>
+        <h1>Joshua Dinh</h1>
+      </div>
       <Nav openProjects={openProjects} />
-      <div className="app__header animate__animated animate__fadeInDownBig"></div>
+      <div className="app__header "></div>
       <div className="app__image-border">
         <img src={img} />
       </div>
