ffe1f2d9db6d3f26425bcd2d21897f5b36a778aa
Joshua Dinh <joshuahungdinh@gmail.com>
2021-04-19 12:41:45 -0700

    updated desc

--- src/Components/Description/DescriptionText.js
+++ src/Components/Description/DescriptionText.js
@@ -13,11 +13,11 @@ export const CovidDesc = () => {
   return (
     <p className="Description-text">
       This application provides statistics and details for the current impact of
-      Coivd19 around the world. Total Cases, Recovery and Deaths are listed in
-      tables in descending order.A drop down list is available to search for
+      Coivd-19 around the world. Total Cases, Recovery and Deaths are listed in
+      tables in a descending order. A drop down list is available to search for
       specific areas of impact. The leaflet map offers users an interactive way
       to view case information and displays colored bubbles in correspondence to
-      the areas statistics.
+      the areas statistical impact.
     </p>
   );
 };
@@ -27,14 +27,21 @@ export const NetflixDesc = () => {
     <p className="Description-text">
       A movie database application inspired by Netflix. The application will
       load Movies & Tv show titles from the Netflix catalog. The program
-      dynamically displays the rows of video's. The user can select a video with
-      an onclick handler. The app will retrieves specific information from TMDB
-      API, store it in Reacts state and use that information with Google's
-      Youtube Api to give the user the ability to view updated trailers.
+      dynamically displays rows of video's. The user can select a video with
+      which will retrieve specific information from TMDB API, store it in Reacts
+      state and then use that information query Google's Youtube Api. This will
+      give the user the ability to view updated trailers.
     </p>
   );
 };
 
 export const YoutubeDesc = () => {
-  return <p className="Description-text">youtube</p>;
+  return (
+    <p className="Description-text">
+      This applciation allows users to query Youtubes data api for video
+      information. Videos that are selected return details based on the videos
+      id. Users will have access not on the play the video but view the
+      author/creator, comments, likes, dislikes and views.
+    </p>
+  );
 };
