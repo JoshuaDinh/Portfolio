b41d793f38c4060aa21f813b94f8634c2f31f149
Joshua Dinh <joshuahungdinh@gmail.com>
2021-04-20 07:26:26 -0700

    removed additional imports and unused animations

--- src/Components/Nav/NavOption.js
+++ src/Components/Nav/NavOption.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./nav.css";
 import { Link } from "react-scroll";
-import { connect } from "react-redux";
-import { toggleAboutMeModal } from "../../actions";
 
 const NavOption = ({ title, link, toggle }) => {
   return (
--- src/Components/OpeningAnimation/OpeningAnimation.js
+++ src/Components/OpeningAnimation/OpeningAnimation.js
@@ -1,38 +1,9 @@
 import React from "react";
 import "./openingAnimation.css";
-import mandala1 from "../../mandala/1.png";
-import mandala2 from "../../mandala/2.png";
-import mandala3 from "../../mandala/3.png";
-import mandala4 from "../../mandala/4.png";
-import mandala5 from "../../mandala/5.png";
-import mandala6 from "../../mandala/6.png";
-import mandala7 from "../../mandala/7.png";
-import mandala8 from "../../mandala/8.png";
-import mandala9 from "../../mandala/9.png";
-import mandala10 from "../../mandala/10.png";
-import mandala11 from "../../mandala/11.png";
-import mandala12 from "../../mandala/12.png";
-import logo from "../../imgs/react-logo.png";
 
 export const OpeningAnimation = () => {
   return (
     <div>
-      {/* <img className="mandala-drop" alt="mandala" src={mandala1} />
-      <img className="mandala-drop" alt="mandala" src={mandala2} />
-      <img className="mandala-drop" alt="mandala" src={mandala3} />
-      <img className="mandala-drop" alt="mandala" src={mandala4} />
-      <img className="mandala-drop" alt="mandala" src={mandala5} />
-      <img className="mandala-drop" alt="mandala" src={mandala6} />
-      <img className="mandala-drop" alt="mandala" src={mandala7} />
-      <img className="mandala-drop" alt="mandala" src={mandala8} />
-      <img className="mandala-drop" alt="mandala" src={mandala9} />
-      <img className="mandala-drop" alt="mandala" src={mandala10} />
-      <img className="mandala-drop" alt="mandala" src={mandala11} />
-      <img className="mandala-drop" alt="mandala" src={mandala12} />
-      <img className="mandala-drop" alt="mandala" src={mandala1} />
-      <img className="mandala-drop" alt="mandala" src={mandala2} />
-      <img className="mandala-drop" alt="mandala" src={mandala3} />
-      <img className="mandala-drop" alt="mandala" src={mandala5} /> */}
       <div className="app__opening-header typewriter">
         <h4>Hello My Name Is</h4>
         <h1>Joshua Dinh</h1>
@@ -40,22 +11,3 @@ export const OpeningAnimation = () => {
     </div>
   );
 };
-
-{
-  /* <img className="mandala-drop" alt="mandala" src={mandala1} />
-      <img className="mandala-drop" alt="mandala" src={mandala2} />
-      <img className="mandala-drop" alt="mandala" src={mandala3} />
-      <img className="mandala-drop" alt="mandala" src={mandala4} />
-      <img className="mandala-drop" alt="mandala" src={mandala5} />
-      <img className="mandala-drop" alt="mandala" src={mandala6} />
-      <img className="mandala-drop" alt="mandala" src={mandala7} />
-      <img className="mandala-drop" alt="mandala" src={mandala8} />
-      <img className="mandala-drop" alt="mandala" src={mandala9} />
-      <img className="mandala-drop" alt="mandala" src={mandala10} />
-      <img className="mandala-drop" alt="mandala" src={mandala11} />
-      <img className="mandala-drop" alt="mandala" src={mandala12} />
-      <img className="mandala-drop" alt="mandala" src={mandala1} />
-      <img className="mandala-drop" alt="mandala" src={mandala2} />
-      <img className="mandala-drop" alt="mandala" src={mandala3} />
-      <img className="mandala-drop" alt="mandala" src={mandala5} /> */
-}
