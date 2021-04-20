e72843f9c70de3e7bf25b4bd49ca7c3adcfa6ad6
Joshua Dinh <joshuahungdinh@gmail.com>
2021-04-20 09:47:27 -0700

    updated with icons

--- src/Components/Technology/Technology.js
+++ src/Components/Technology/Technology.js
@@ -5,7 +5,7 @@ export const Technology = ({ SkillsUsedText }) => {
   return (
     <div className="tech-used">
       <h4>Technologies & skills</h4>
-      {SkillsUsedText}
+      <div> {SkillsUsedText}</div>{" "}
     </div>
   );
 };
--- src/Components/Technology/TechnologyText.js
+++ src/Components/Technology/TechnologyText.js
@@ -1,18 +1,42 @@
+import "./technology.css";
+import reactIcon from "../../imgs/react-logo.png";
+import reduxIcon from "../../imgs/redux-icon.png";
+import jsIcon from "../../imgs/js-icon.png";
+import chartjsIcon from "../../imgs/chartjs-icon.svg";
+import npmIcon from "../../imgs/npm-icon.png";
+import coinGeckoIcon from "../../imgs/coingecko-icon.png";
+import bloombergIcon from "../../imgs/bloomberg-icon.png";
+import googleIcon from "../../imgs/google-icon.png";
+
 export const BitcoinSkills = () => {
   return (
     <div className="techSkills-text-container">
-      <ul>
-        <li className="techSkills-text">React / hooks</li>
-        <li className="techSkills-text">Javascript ES6+</li>
-        <li className="techSkills-text">Chart.Js</li>
-        <li className="techSkills-text">Coingecko Api</li>
-        <li className="techSkills-text">Bloomberg Api</li>
-        <li className="techSkills-text">JSX</li>
-        <li className="techSkills-text">HTML / CSS</li>
-      </ul>
+      <img className="techSkills-icon" src={reactIcon} />
+      <img className="techSkills-icon" src={reduxIcon} />
+      <img className="techSkills-icon" src={jsIcon} />
+      <img className="techSkills-icon" src={chartjsIcon} />
+      <img className="techSkills-icon" src={npmIcon} />
+      <img className="techSkills-icon" src={googleIcon} />
+      <img className="techSkills-icon" src={coinGeckoIcon} />
+      <img className="techSkills-icon" src={bloombergIcon} />
     </div>
   );
 };
+// export const BitcoinSkills = () => {
+//   return (
+//     <div className="techSkills-text-container">
+//       <ul>
+//         <li className="techSkills-text">React / hooks</li>
+//         <li className="techSkills-text">Javascript ES6+</li>
+//         <li className="techSkills-text">Chart.Js</li>
+//         <li className="techSkills-text">Coingecko Api</li>
+//         <li className="techSkills-text">Bloomberg Api</li>
+//         <li className="techSkills-text">JSX</li>
+//         <li className="techSkills-text">HTML / CSS</li>
+//       </ul>
+//     </div>
+//   );
+// };
 
 export const CovidSkills = () => {
   return (
