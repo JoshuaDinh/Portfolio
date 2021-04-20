ff48aaf942a1fadf6d57caf30e612dc1afcc8cc9
Joshua Dinh <joshuahungdinh@gmail.com>
2021-04-19 20:33:49 -0700

    added personal icons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,13 @@ import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import { connect } from "react-redux";
 import { toggleMobileNav } from "../../actions";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import PictureAsPdfIcon from "@material-ui/icons/PictureAsPdf";
+import PhoneIcon from "@material-ui/icons/Phone";
+import Tooltip from "@material-ui/core/Tooltip";
+import Zoom from "@material-ui/core/Zoom";
+import IconButton from "@material-ui/core/IconButton";
 
 import "./header.css";
 
@@ -31,7 +38,26 @@ const Header = ({ toggleMobileNav }) => {
             <div className="menu-icon"></div>
             <div className="menu-icon"></div>
           </div>
-          <Avatar className="header-avatar-icon">JD</Avatar>
+          <Tooltip title="619-977-3574" TransitionComponent={Zoom} arrow>
+            <IconButton aria-label="contact">
+              <PhoneIcon className="header-icon" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Github" TransitionComponent={Zoom} arrow>
+            <IconButton aria-label="delete">
+              <GitHubIcon className="header-icon" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Linked-in" TransitionComponent={Zoom} arrow>
+            <IconButton aria-label="linked-in">
+              <LinkedInIcon className="header-icon" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Resume" TransitionComponent={Zoom} arrow>
+            <IconButton aria-label="resume">
+              <PictureAsPdfIcon className="header-icon" />
+            </IconButton>
+          </Tooltip>
         </div>
       </div>
     </div>
