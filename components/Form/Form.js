97c422da09678a065a65145c922526df8d9973f1
Joshua Dinh <joshuahungdinh@gmail.com>
2022-02-09 15:33:46 -0800

    added form and functionality  to enable messages to be sent with node mailer

---
+++ components/Form/Form.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import styles from "./Form.module.css";
+import axios from "axios";
+import SendIcon from "@mui/icons-material/Send";
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    message: "",
+    email: "",
+  });
+
+  const { name, message, email } = formData;
+
+  // Update formData based off of name attributes on input fields
+  const updateFormData = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const submitMessage = async (e, formData) => {
+    e.preventDefault();
+    try {
+      await axios.post("/api/contact", formData);
+      // setMsgSuccess(true);
+      alert("Message Sent Succesfully");
+      setFormData({ name: "", email: "", message: "" });
+    } catch (err) {
+      if (err) {
+        // setMsgSuccess(true);
+        alert("Message Failed");
+      }
+    }
+  };
+  console.log(formData);
+  return (
+    <form className={styles.form} onSubmit={(e) => submitMessage(e, formData)}>
+      <label className={styles.label}>Name</label>
+      <input
+        className={styles.input}
+        onChange={(e) => updateFormData(e)}
+        name="name"
+        value={name}
+        type="text"
+        placeholder="Name"
+      />
+      <label className={styles.label}>Email</label>
+      <input
+        className={styles.input}
+        onChange={(e) => updateFormData(e)}
+        name="email"
+        value={email}
+        type="email"
+        placeholder="Email"
+      />
+      <label className={styles.label}>Message</label>
+      <textarea
+        className={styles.textArea}
+        name="message"
+        value={message}
+        onChange={(e) => updateFormData(e)}
+        placeholder="Enter your message"
+      />
+      <div className={styles.buttonWrapper}>
+        <button
+          className={styles.button}
+          onSubmit={(e) => submitMessage(e, formData)}
+        >
+          Submit
+          <SendIcon className={styles.icon} />
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
