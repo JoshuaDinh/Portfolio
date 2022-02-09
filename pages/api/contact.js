ce3eb6b36ef2710471e0bcf2cb4dc348d4bea1a8
Joshua Dinh <joshuahungdinh@gmail.com>
2022-02-09 15:31:47 -0800

    added nodemailer functionality to send messages through contact form

---
+++ pages/api/contact.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+export default function (req, res) {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  const mailData = {
+    from: process.env.EMAIL_FROM,
+    to: process.env.EMAIL,
+    subject: `Message From ${req.body.name}`,
+    text: req.body.message + " | Sent from: " + req.body.email,
+    html: `<div>${req.body.message}</div><p>Sent from:
+      ${req.body.email}</p>`,
+  };
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
+  res.status(200);
+}
